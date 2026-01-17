4237c74aaedb710989c953ccaf99a2121ba28702
Roman Pshenichnikov <pshenichnikov.r@gmail.com>
2026-01-17 17:03:40 +0300

    Initial Commit

---
+++ OpenWebUIDesktop/OpenWebUIDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWebUIDesktopApp.swift
+//  OpenWebUIDesktop
+//
+//  Created by Roman on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenWebUIDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
